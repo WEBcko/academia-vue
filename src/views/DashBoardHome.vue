994ee90a4bc8d444aa74363d7513804dfedfbf2a
emilioanastacio <emilioanastacio@gmail.com>
2023-06-16 20:22:54 -0300

    começo da dashboard

---
+++ src/views/DashBoardHome.vue
@@ -0,0 +1,35 @@
+<template>
+
+<NavBar></NavBar>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import NavBar from '@/components/NavBar.vue'; // @ is an alias to /src
+import SideBar from '@/components/SideBar.vue';
+
+export default defineComponent({
+  name: 'DashBoardHome',
+  components: {
+    NavBar,
+    SideBar
+},
+});
+</script>
+
+<style scoped>
+    .dashboard{
+        display: grid;
+        grid-template-columns: 1fr 5fr;
+        background-color: aquamarine;
+        height: 100vh;
+        width: 100%;
+    }
+    .content{
+        background-color: azure;
+        border-radius: 18px;
+        margin: 1% 1% 1% 0%;
+    }
+
+</style>
